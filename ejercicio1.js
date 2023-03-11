a66ced9c410e545f5f80ed9034d6edc0793a30b9
Ramirez Brian <tiziano0705@hotmail.com>
2023-03-11 11:19:01 -0300

    ejericio 1

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,59 +1,67 @@
-listProducts=[
-    {
-        id: 01,
-        title:"producto prueba3",
-        description:"Este es un producto prueba",
-        price:200,
-        thumbnail:"Sin imagen",
-        code:"001",
-        stock:25
-    }
-]
-
 
 class ProductManager{
     constructor(){
-    this.products = listProducts;}
+    this.products = [];}
 
 getProducts(){
         return this.products;
 }
 
 addProduct(newProd){
+    const keysValidator = ["id","title","description","price","thumbnail","code","stock"];
+    
+
     const prod = this.products.find(product => product.code === newProd.code)
     if (prod) {
         return "Product already exists with code "
     }
-
+    
     if (this.products.length === 0) {
         this.products.push( {id: 1, ...newProd } )
     } else {
         this.products.push( {id: this.products[this.products.length-1].id + 1  , ...newProd } )
     }
-    }
+    
+    const producto =this.products.forEach( (producto) =>console.log(Object.keys(producto)));
+    console.log(keysValidator)
+    console.log(producto);
+
+
+    const isEqual = (keysValidator.length === producto.length) &&
+            keysValidator.every((value, index) => value === producto[index])
+
+    console.log(isEqual);
+    
+    
+
+
+    // console.log(Object.keys(keysValidator))
+    // // console.log(Object.keys(products))
+    // console.log(this.products);
+}
 
 
 
 getProductById(id){
-    const prod = this.products;
-    const product = prod.find(x => x.id === id)
+    const product = this.products.find(x => x.id === id)
 
-    if(product.id !== id){
+    if(!product){
     console.log("Id not found");
     }else{
-        return product;
+        return;
     }
+        return product
     }
 }
 
 const manegeProducts = new ProductManager()
 
 console.log(manegeProducts.addProduct({
-    title:"producto prueba2",
-    description:"Este es un producto prueba2",
-    price:200,
-    thumbnail:"Sin imagen",
-    code:"002",
-    stock:25
+   title: "producto prueba2",
+   description: "Este es un producto prueba2",
+   price:200,
+   thumbnail: "Sin imagen",
+   code: "002",
+   stock: 125
 }));
 console.log(manegeProducts.getProducts());
